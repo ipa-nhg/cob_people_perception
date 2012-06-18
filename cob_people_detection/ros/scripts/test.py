0d75669e71f7f8d283ba98d38d9dc6d3bdab0dd8
nhg-ipa <Nadia.Hammoudeh.Garcia@ipa.fraunhofer.de>
2012-06-18 14:10:55 +0200

    new test script for cob_people_perception

--- cob_people_detection/ros/scripts/test.py
+++ cob_people_detection/ros/scripts/test.py
@@ -14,6 +14,7 @@ from simple_script_server import *
 sss = simple_script_server()
 
 from cob_people_detection_msgs.msg import *
+from cob_people_detection.msg import *
 from cob_people_detection.srv import *
 
 
@@ -21,34 +22,60 @@ class DetectPeopleScript(script):
 		
 	def Initialize(self):
 		self.srv_people_detection = '/cob_people_detection/detect_people'
+		self.srv_recognize = '/cob_people_detection/face_detection/recognize_service_server'
 
 		
-	def Run(self): 
-
-
+	def Run(self):
 		try:
-			rospy.wait_for_service(self.srv_people_detection,10)
+			rospy.wait_for_service(self.srv_recognize,10)
 		except rospy.ROSException, e:
 			print "Service not available: %s"%e
 
 		# call people detection service
 		try:
-			detector_service = rospy.ServiceProxy(self.srv_people_detection, DetectPeople)
-			req = DetectPeopleRequest()
-			res = detector_service(req)
+			recognize_service = rospy.ServiceProxy(self.srv_recognize, Recognition)
+			req1 = RecognitionRequest()
+			req1.running == True
+			req1.doRecognition == True
+			req1.display == True
+			res1 = recognize_service(req1)
 		except rospy.ServiceException, e:
 			print "Service call failed: %s"%e
-			self.retries = 0
-			return 'failed'
+			return 'failed' 
+		#while (True):
+		for i in range (1,500):
+			try:
+				rospy.wait_for_service(self.srv_people_detection,10)
+			except rospy.ROSException, e:
+				print "Service not available: %s"%e
+
+			# call people detection service
+			try:
+				detector_service = rospy.ServiceProxy(self.srv_people_detection, DetectPeople)
+				req = DetectPeopleRequest()
+				res = detector_service(req)
+			except rospy.ServiceException, e:
+				print "Service call failed: %s"%e
+				return 'failed'
 			
-		# check for no objects
-		if len(res.people_list.detections) <= 0:
-			rospy.logerr("No faces found")
-			self.retries += 1
+			# check for no objects
+			if len(res.people_list.detections) <= 0:
+				rospy.logerr("No faces found")
 
 
-		for item in res.people_list.detections:
-			sss.say(["Hi " + item.label + "."],False)
+			for item in res.people_list.detections:
+				sss.say(["Hi " + item.label + "."],False)
+
+		try:
+			recognize_service = rospy.ServiceProxy(self.srv_recognize, Recognition)
+			req2 = RecognitionRequest()
+			req2.running == False
+			req2.doRecognition == False
+			req2.display == False
+			res2 = recognize_service(req2)
+		except rospy.ServiceException, e:
+			print "Service call failed: %s"%e
+			return 'failed' 
 
 		
 	
